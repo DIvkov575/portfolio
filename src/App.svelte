50ff72b1b4680f9af4a94b21b5989ac67b40a828
Divkov575 <dmitriy@ivkov.net>
2024-06-17 18:15:48 -0400

    working d1

--- src/App.svelte
+++ src/App.svelte
@@ -8,13 +8,21 @@
 
     <div class="full-width-section-black" id="d1">
         <h1 class="heading-1">Dmitriy Ivkov</h1>
-            <div id="description-wrapper">
-                <h3>asdfkas;ldfja;lksdjfl;kasdjf;ljasdl;fkasl;dfkj</h3>
-                <h3>asdfkas;ldfja;lksdjfl;kasdjf;ljasdl;fkasl;dfkj</h3>
-            </div>
-            <div id="profile-picture-wrapper">
-                <img src="../public/me1.JPEG" alt="">
-            </div>
+        <div id="description-wrapper">
+            <h3>Hi, I'm Dmitriy 👋</h3>
+            <h3>SWE Intern & Senior at Belmont High School</h3>
+        </div>
+        <div id="profile-picture-wrapper">
+            <img src="../public/me1.jpg" alt="">
+        </div>
+    </div>
+
+    <div class="clear-wrapper" id="d2">
+        <ul>
+            <li>Currently Lear</li>
+        </ul>
     </div>
 
 </main>
+<!--            <h3>I love Computer Science and Distributed Compute systems</h3>-->
+<!--            <h3>I'm currently a senior at Belmont High School</h3>-->
