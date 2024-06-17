d8d9c74ca7f3f21c2afb9e01e05e99360b72c8a4
Divkov575 <dmitriy@ivkov.net>
2024-06-16 23:24:32 -0400

    optimzized me.jpg & created circular headshot

--- src/App.svelte
+++ src/App.svelte
@@ -6,8 +6,15 @@
 
 <main>
 
-    <div class="full-width-section-black">
-        a;sdkfj
+    <div class="full-width-section-black" id="d1">
+        <h1 class="heading-1">Dmitriy Ivkov</h1>
+            <div id="description-wrapper">
+                <h3>asdfkas;ldfja;lksdjfl;kasdjf;ljasdl;fkasl;dfkj</h3>
+                <h3>asdfkas;ldfja;lksdjfl;kasdjf;ljasdl;fkasl;dfkj</h3>
+            </div>
+            <div id="profile-picture-wrapper">
+                <img src="../public/me1.JPEG" alt="">
+            </div>
     </div>
 
 </main>
