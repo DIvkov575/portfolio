e7721e0b22865d1a2f8cd53be792a8f015456263
Divkov575 <dmitriy@ivkov.net>
2024-06-18 11:34:19 -0400

    this shit ass, ill publish it ig

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,5 @@
 <script>
 import svelteLogo from './assets/svelte.svg'
-  let animate = false;
-  function handleClick(event) {
-      animate = true;
-      window.open("https://docs.google.com/document/d/1T9XzrENQ6zIA31F6u62IVUZ4Nv0bLxrnX-tp452k9Yw/edit?usp=sharing")
-
-  }
 </script>
 
 
@@ -15,12 +9,9 @@ import svelteLogo from './assets/svelte.svg'
         <h1 class="heading-1">Dmitriy Ivkov</h1>
         <div id="description-wrapper">
             <h3>🦀, Data, Pure Math, and Computer Science</h3>
-<!--            <h3>SWE Intern & Senior at Belmont High School</h3>-->
         </div>
         <div id="link-wrapper">
-            <h3 on:click={handleClick}>Resume</h3>
-            <div class:animate-circle={animate} class="animation-circle"></div>
-<!--            <a target ="_blank" on:click={handleClick} href=""><h3>Resume</h3></a>-->
+            <a target ="_blank"  href="https://docs.google.com/document/d/1T9XzrENQ6zIA31F6u62IVUZ4Nv0bLxrnX-tp452k9Yw/edit?usp=sharing"><h3>Resume</h3></a>
             <a target ="_blank" href="https://www.linkedin.com/in/dmitriyivkov/"><h3>Linkedin</h3></a>
             <a target ="_blank" href="https://github.com/DIvkov575"><h3>Github</h3></a>
         </div>
@@ -36,28 +27,33 @@ import svelteLogo from './assets/svelte.svg'
         <div id="image-wrapper">
             <img src="https://github-readme-stats.vercel.app/api?username=Divkov575&theme=swift&rank_icon=github&card_width=400">
             <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Divkov575&layout=compact&theme=swift&card_width=355">
+<!--            <img src="https://github-readme-stats.vercel.app/api?username=Divkov575&theme=nord&rank_icon=github&card_width=400">-->
+<!--            <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Divkov575&layout=compact&theme=nord&card_width=355">-->
         </div>
         <div id="text-wrapper">
-            <h3 style="font-weight: 600">Hi, I'm Dima 👋</h3>
-            <h3 style="font-weight: 600">I'm a Software Engineer in the Boston Area and a rising Senior at Belmont High School.
+            <h3>Hi, I'm Dima 👋</h3>
+            <h3>I'm a Software Engineer in the Boston Area and a rising Senior at Belmont High School.
                 I have a passion for Pure-Math, Statistics, and Computer Science.
                 I enjoy using my skills to write concurrent algorithms and to work with distributed compute systems.
-                In my free time, I enjoy studying Pure-Math, programming, and DH mountain biking.
+                In my free time, I like studying Pure-Math, coding, and DH mountain biking.
             </h3>
             <br>
-            <u><h3 style="font-weight: 600">I'm Currently</h3></u>
+            <u><h3>I'm Currently</h3></u>
             <h3>Learning: Information Theory</h3>
             <h3>Building: SIEM and security Data-lake (on GCP in Rust)</h3>
             <h3>Grinding: College Applications</h3>
             <h3>Reading: "Information Retrieval" by Grossman, Frieder (Springer)</h3>
             <br>
-            <u><h3 style="font-weight: 600">My Setup</h3></u>
+            <u><h3>My Setup</h3></u>
             <h3>Language: Rust</h3>
             <h3>Cloud Provider: Google-Cloud-Provider</h3>
             <h3>Operating Systems: Macos / Debian</h3>
         </div>
     </div>
 
+    <div class="clear-wrapper">
+        <h3>I might post what ive been learning here</h3>
+        <div></div>
+    </div>
+
 </main>
-<!--            <h3>I love Computer Science and Distributed Compute systems</h3>-->
-<!--            <h3>I'm currently a senior at Belmont High School</h3>-->
