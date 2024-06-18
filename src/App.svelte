2f331003d262a43e9d058ef1bed821f19b265651
Divkov575 <dmitriy@ivkov.net>
2024-06-18 10:24:55 -0400

    workign contents - beginning to add animatinos

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,15 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+
+  import { onMount } from 'svelte'
+  let animate = false;
+  function handleClick(event) {
+      animate = false;
+      setTimeout(() => {
+          animate = true;
+      }, 5)
+  }
 </script>
 
 <main>
@@ -9,8 +18,13 @@
     <div class="full-width-section-black" id="d1">
         <h1 class="heading-1">Dmitriy Ivkov</h1>
         <div id="description-wrapper">
-            <h3>Hi, I'm Dmitriy 👋</h3>
-            <h3>SWE Intern & Senior at Belmont High School</h3>
+            <h3>🦀, Data, Pure Math, and Computer Science</h3>
+<!--            <h3>SWE Intern & Senior at Belmont High School</h3>-->
+        </div>
+        <div id="link-wrapper">
+            <a target ="_blank" on:click={handleClick} href="https://docs.google.com/document/d/1T9XzrENQ6zIA31F6u62IVUZ4Nv0bLxrnX-tp452k9Yw/edit?usp=sharing"><h3>Resume</h3></a>
+            <a target ="_blank" href="https://www.linkedin.com/in/dmitriyivkov/"><h3>Linkedin</h3></a>
+            <a target ="_blank" href="https://github.com/DIvkov575"><h3>Github</h3></a>
         </div>
         <div id="profile-picture-wrapper">
             <img src="../public/me1.jpg" alt="">
@@ -18,9 +32,29 @@
     </div>
 
     <div class="clear-wrapper" id="d2">
-        <ul>
-            <li>Currently Lear</li>
-        </ul>
+        <div id="image-wrapper">
+            <img src="https://github-readme-stats.vercel.app/api?username=Divkov575&theme=swift&rank_icon=github&card_width=400">
+            <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Divkov575&layout=compact&theme=swift&card_width=355">
+        </div>
+        <div id="text-wrapper">
+            <h3 style="font-weight: 600">Hi, I'm Dima 👋</h3>
+            <h3 style="font-weight: 600">I'm a Software Engineer in the Boston Area and a rising Senior at Belmont High School.
+                I have a passion for Pure-Math, Statistics, and Computer Science.
+                I enjoy using my skills to write concurrent algorithms and to work with distributed compute systems.
+                In my free time, I enjoy studying Pure-Math, programming, and DH mountain biking.
+            </h3>
+            <br>
+            <u><h3 style="font-weight: 600">I'm Currently</h3></u>
+            <h3>Learning: Information Theory</h3>
+            <h3>Building: SIEM and security Data-lake (on GCP in Rust)</h3>
+            <h3>Grinding: College Applications</h3>
+            <h3>Reading: "Information Retrieval" by Grossman, Frieder (Springer)</h3>
+            <br>
+            <u><h3 style="font-weight: 600">My Setup</h3></u>
+            <h3>Language: Rust</h3>
+            <h3>Cloud Provider: Google-Cloud-Provider</h3>
+            <h3>Operating Systems: Macos / Debian</h3>
+        </div>
     </div>
 
 </main>
